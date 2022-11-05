dd89f4308c4d94409e7d2a7978a94538a6b3d05d
Muhammad Asif <alexasifhhossain@gmail.com>
2022-11-05 21:10:31 +0600

    python variables added

---
+++ basic/variables.py
@@ -0,0 +1,19 @@
+name = 'Asif' # string data type
+price = 50 # integer data type
+rating = 4.8 # float data type
+is_active = True # boolean data type
+# print(name)
+# print(price)
+# print(rating)
+# print(is_active)
+
+patient_name = 'John Smith'
+patient_age = 20
+new_patient = True
+print(f"patient name is {patient_name}, age is {patient_age} and {new_patient}")
+
+# Take input from users
+name = input("your name: ")
+color = input("your fav color: ")
+print(f"{name} likes {color}")
+
