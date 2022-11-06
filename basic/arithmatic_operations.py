dc7b14f9fb5d8b397117061cba958768f8f271a2
Muhammad Asif <alexasifhhossain@gmail.com>
2022-11-06 23:46:27 +0600

    math module, operations,functions,precedence

---
+++ basic/arithmatic_operations.py
@@ -0,0 +1,37 @@
+# print(10 + 3)
+# print(10 -3)
+# print(10 * 3)
+# print(10 / 3) #return 3.3333333333333335
+
+# print(10 // 3) # return 3
+# print(10 // 4) # return 2
+
+# print(10 % 3) # return 1
+# print(10 ** 3) #return 1000, power of 3
+
+# a =  20
+# a = a + 5 # a += 5
+# print(a) # return 25
+
+                    # OPERATOR PRECEDENCE
+# priority get high
+# 1. exponentiation 2**3
+# 2. multiplication or division
+# 3. addition or substraction
+# x = 10 + 5 * 2
+# print(x) #return 20
+
+# y = 10 + 3 * 2 ** 2
+# print(y) # retur 22
+
+# z = (10 + 3) * 2 ** 2
+# print(z) # return 52, because the parenthesis number will get priority
+
+# b = (2 + 3) * 10 - 3
+# print(b) # return 47
+
+                        # MATH FUNCTIONS
+print(round(2.9)) # return a complete number 3
+print(abs(-2.9)) #absolute always return a positive number
+#search python math module, it has a library math, same as javascript ceil(), floor()
+
