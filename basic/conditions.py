ee344dc88f017cb2685c4acaf1c0c1c2ad981b0c
Muhammad Asif <alexasifhhossain@gmail.com>
2022-11-08 00:27:49 +0600

    comparision, arithmatic, logical added

--- basic/conditions.py
+++ basic/conditions.py
@@ -18,3 +18,38 @@ elif buyer_bad_credit:
 
 else:
     print('print the down payment')
+
+                        #COMPARISION OPERATORS
+temp = 5
+
+if temp > 30:
+    print("it's a hot day")
+
+elif temp < 10:
+    print("It's a cold day")
+
+else:
+    print("It's neither hot nor cold")
+
+                        #lOGICAL OPERATOR
+
+has_credit = False
+has_money = False
+
+if has_credit and has_money:
+    print('You are eligible')
+
+elif has_credit and not has_money:
+    print('you are Poor')
+
+elif not has_credit and has_money:
+    print('you are rich')
+
+elif has_credit or has_money:
+    print('You need support')
+
+else:
+    print('you are eliminated')
+
+
+
