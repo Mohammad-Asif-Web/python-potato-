23a1033ba4e5dbcdb96b2de0aa349d5c2a870d03
Muhammad Asif <alexasifhhossain@gmail.com>
2022-11-08 00:26:54 +0600

    name lenght test

---
+++ task/character_lenght.py
@@ -0,0 +1,12 @@
+name = 'Muhammad Asif'
+
+if len(name) < 3:
+    print('Name need atleast 3 character')
+
+elif len(name) > 50:
+    print('Maximum character limit is 50')
+
+else:
+    print('Name looks good!')
+
+
