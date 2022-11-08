f737f3e839ed4b675dfadb9955d9e540660bc75a
Muhammad Asif <alexasifhhossain@gmail.com>
2022-11-08 21:05:53 +0600

    weight converter added

---
+++ task/weight_converter.py
@@ -0,0 +1,13 @@
+weight = int(input('Weight: '))
+unit = input('(L)bs or (k)g: ')
+convert = ''
+
+if unit.upper() == 'L':
+    convert = weight * 0.45
+    print(f"you are {convert} kilos")
+
+else:
+    convert = weight // 0.45
+    print(f"You are {convert} pounds")
+
+
