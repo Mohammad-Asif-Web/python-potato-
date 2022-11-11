3a1f20036e391b89c5c5c200edd3e2e8eb7903c1
Muhammad Asif <alexasifhhossain@gmail.com>
2022-11-11 10:49:24 +0600

    while, for, list loop, range loop

---
+++ basic/loops.py
@@ -0,0 +1,25 @@
+                            #while loop     
+i = 1
+while i<=5:
+    print('*' * i)
+    i += 1
+print('Done')
+
+                            # For loops
+items = "python potato"
+for item in items:
+    print(item)
+
+lists = ['Asif', 'Khan', 'john']
+for name in lists:
+    print(name) # return single string asif, khan, john
+
+for num in range(10):
+    print(num) #return single num - 0, 1, 2, 3, 4, 5,,,,,, 10
+
+
+
+
+
+
+
